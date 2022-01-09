dd2a2846a16c585b4532f647e6bb64af5393c76e
Neda Davoudi <53782756+ndavoudi@users.noreply.github.com>
2022-01-09 23:54:08 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,8 +1,15 @@
 import numpy
-import matplotlib.pyplot
+import matplotlib
+import matplotlib.pyplot as plt
 
 print('Neda')
 sampl = numpy.random.uniform(low=0.5, high=13.3, size=(5,))
 x = numpy.array([1, 2, 3, 4, 5])
-matplotlib.pyplot.plot(x,sampl)
-matplotlib.pyplot.show()
+#matplotlib.pyplot.plot(x,sampl)
+#matplotlib.pyplot.show()
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(range(100))
+
+fig.savefig('graph.png')
